ea02486d092c48fd1e9d194b9081bb27d3e68d36
Jom Gapuz <bark@jomgapuz.com>
2021-04-09 20:15:47 +0800

    add master key for dashboard to work

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const config = {
   databaseURI: databaseUri || 'mongodb://root:example@localhost:27017/parse?authSource=admin',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
